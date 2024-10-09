32adf045ef11976da8646013e8b1617b870fa8f3
vignesh <vigneshvicky242002@gmail.com>
2024-10-09 21:53:20 +0530

    Bug Fixing: Mobile view Bug fixed

--- indian_fertilizer/settings.py
+++ indian_fertilizer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-nz7vhat23u1s0xkf%2g)tfj0_h_9!*nm^^a3(f^b$mz@@i+n$a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
