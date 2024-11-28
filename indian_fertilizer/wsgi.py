c4553aa2c5fdc802e8cf9d693fb14ffd9612f083
senthilmukeshhari <r.senthilkumar22bca106@gmail.com>
2024-11-28 21:34:35 +0530

    some

--- indian_fertilizer/wsgi.py
+++ indian_fertilizer/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'indian_fertilizer.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'indian_fertilizer.settings.base')
 
 application = get_wsgi_application()
